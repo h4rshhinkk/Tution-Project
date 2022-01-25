5e722ed3ce58b1062a73bda2aceaa43acab4afa3
harshinsgit5060 <h4shhin@gmail.com>
2022-01-25 15:27:55 +0530

    verherokudeploy

--- tutionproject/tutionproject/settings.py
+++ tutionproject/tutionproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%7an&k7r_c906ht7dzmw2&1v1v^r-px63)w3pt7tf*-5$p14v@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
